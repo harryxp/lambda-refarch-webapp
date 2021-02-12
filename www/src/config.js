216aedb5dd3b31e10d42c011208271a6b6a17a45
Harry Pan <harrpan@amazon.com>
2021-02-12 13:24:18 -0800

    correct stage

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "7jn0k0tml175h73rod5ufv01bd",     // CognitoClientID
-  "api_base_url": "https://tgtwzhjfle.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "api_base_url": "https://tgtwzhjfle.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "mytodoappdemo-lambda-refarch-webapp-sam-cli.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d1q389a52ymlfh.amplifyapp.com"                                      // AmplifyURL
 };
